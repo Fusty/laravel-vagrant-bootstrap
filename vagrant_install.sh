c80effb05af930784e71c097fdf294f9712a9d68
Michael Bender <michaelbenbender@gmail.com>
2014-01-22 12:41:47 -0500

    Clean up vagrant file. Use composer for laravel install instead of git

--- vagrant_install.sh
+++ vagrant_install.sh
@@ -1,122 +1,70 @@
 #!/usr/bin/env bash
 
-echo "--- Good morning, master. Let's get to work. Installing now. ---"
-
-echo "--- Updating packages list ---"
 sudo apt-get update
-
-echo "--- MySQL time ---"
-sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password root'
-sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password root'
-
-echo "--- Installing base packages ---"
 sudo apt-get install -y vim curl python-software-properties python g++ make
-
-echo "--- Updating packages list ---"
-sudo apt-get update
-
-echo "--- We want the bleeding edge of PHP, right master? ---"
 sudo add-apt-repository -y ppa:ondrej/php5
 sudo add-apt-repository -y ppa:chris-lea/node.js
-
-echo "--- Updating packages list ---"
 sudo apt-get update
 
 echo "--- Installing PHP-specific packages ---"
-sudo apt-get install -y php5 apache2 libapache2-mod-php5 php5-curl php5-gd php5-mcrypt mysql-server-5.5 php5-mysql git-core
+sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password root'
+sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password root'
+sudo apt-get install -y php5 apache2 libapache2-mod-php5 php5-curl php5-gd php5-mcrypt mysql-server-5.5 php5-mysql git-core php5-xdebug nodejs
 
-echo "--- Installing and configuring Xdebug ---"
-sudo apt-get install -y php5-xdebug
-
-echo "--- Installing and configuring nodjs and npm --- "
-sudo apt-get install -y nodejs
+echo "--- Configure PHP and xDebug ---"
+sudo sed -i '$a xdebug.scream=1' /etc/php5/mods-available/xdebug.ini
+sudo sed -i '$a xdebug.cli_color=1' /etc/php5/mods-available/xdebug.ini
+sudo sed -i '$a xdebug.show_local_vars=1' /etc/php5/mods-available/xdebug.ini
+sudo sed -i "s/error_reporting = .*/error_reporting = E_ALL/" /etc/php5/apache2/php.ini
+sudo sed -i "s/display_errors = .*/display_errors = On/" /etc/php5/apache2/php.ini
 
 echo "--- Installing and configuring grunt and bower globally ---"
 sudo npm install -g grunt-cli
 sudo npm install -g bower
 
-
-sed -i '$a xdebug.scream=1' /etc/php5/mods-available/xdebug.ini
-sed -i '$a xdebug.cli_color=1' /etc/php5/mods-available/xdebug.ini
-sed -i '$a xdebug.show_local_vars=1' /etc/php5/mods-available/xdebug.ini
-
-
-echo "--- Enabling mod-rewrite ---"
-sudo a2enmod rewrite
-
-
-
-
-echo "--- What developer codes without errors turned on? Not you, master. ---"
-sudo sed -i "s/error_reporting = .*/error_reporting = E_ALL/" /etc/php5/apache2/php.ini
-sudo sed -i "s/display_errors = .*/display_errors = On/" /etc/php5/apache2/php.ini
-
-sudo sed -i "s/AllowOverride None/AllowOverride All/" /etc/apache2/apache2.conf
-
-echo "--- Restarting Apache ---"
-sudo service apache2 restart
-
-echo "--- Composer is the future. But you knew that, did you master? Nice job. ---"
+echo "--- Install Composer ---"
 curl -sS https://getcomposer.org/installer | php
 sudo mv composer.phar /usr/local/bin/composer
 
-# Install Ruby
-sudo \curl -L https://get.rvm.io | bash
-source /etc/profile.d/rvm.sh
+echo "--- Install Ruby through RVM ---"
+\curl -L https://get.rvm.io | bash
+source /home/vagrant/.rvm/scripts/rvm
 rvm install 1.9.3
 rvm use 1.9.3
 
-# Laravel stuff here, if you want
+echo "--- Add project specific gems ---"
+gem install sass
+gem install compass
 
-echo "--- Make a new Laravel project ---"
+echo "--- Install project's packages ---"
+sudo chown -R vagrant:vagrant /home/vagrant/tmp
+cd /vagrant
 
-if [ ! -f /vagrant/composer.json ]
-  then
-    git clone https://github.com/laravel/laravel.git
-    cd laravel
-    rm -rf .git
-    mkdir app/assets
-    mkdir app/assets/css
-    mkdir app/assets/js
-    mkdir app/assets/images
-    mkdir app/assets/fonts
-    touch app/assets/css/main.scss
-    tar pcf - .| (cd /vagrant/; tar pxf -)
-    cd ..
-    rm -rf laravel
-    cd /vagrant
-    composer require mccool/laravel-auto-presenter:*
-    composer require --dev way/generators:*
-    composer install --prefer-dist
-    composer dump-autoload
+if [ -f /vagrant/composer.json ]
+    then
+        cd /vagrant
+        composer update
 fi
 
-echo "--- Install node packages ---"
-cd /vagrant
-sudo chown -R vagrant:vagrant /home/vagrant/tmp
-sudo chmod -R 755 app/storage
+if [ -f /vagrant/package.json ]
+    then
+        cd /vagrant
+        npm install
+fi
 
-sudo gem install sass
-sudo gem install compass
-npm install
+if [ -f /vagrant/bower.json ]
+    then
+        cd /vagrant
+        bower cache clean
+        bower install
+fi
 
-echo "--- Install bower packages ---"
-cd /vagrant
-bower install --allow-root
+echo "--- Laravel specific settings ---"
+chmod -R 755 /vagrant/app/storage
 
-echo "--- Update .gitignore ---"
-sed -i '$a bower_components' .gitignore
-sed -i '$a node_modules' .gitignore
-
-echo "--- Setting document root ---"
+echo "--- Configure Apache ---"
+sudo a2enmod rewrite
+sudo sed -i "s/AllowOverride None/AllowOverride All/" /etc/apache2/apache2.conf
 sudo rm -rf /var/www
 sudo ln -fs /vagrant/public /var/www
-
-# Cleanup
-cd /vagrant
-rm -rf .git
-rm .gitattributes
-rm Readme.md
-
-grunt watch
-echo "--- All set to go! Would you like to play a game? ---"
+sudo service apache2 restart
